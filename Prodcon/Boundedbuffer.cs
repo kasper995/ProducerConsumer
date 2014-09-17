cada8a11c836fd6cd275a567289b4eb03203a8a8
kasper995 <kasper995@hotmail.dk>
2014-09-17 13:42:51 +0200

    main

---
+++ Prodcon/Boundedbuffer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prodcon
+{
+    class Boundedbuffer
+    {
+        public Queue<int> queue = new Queue<int>();
+
+        private int BoundedBuffer;
+        public bool IsFull()
+        {
+            
+            if (BoundedBuffer <= 4)
+            {
+                return false;
+            }
+            
+        else
+            {
+                return true;
+            }
+       }
+
+        public int Put()
+        {
+            queue.Enqueue(0);
+        }
+
+        public int Take()
+        {
+            queue.Dequeue();
+        }
+
+    
+
+    }
+}
---
+++ Prodcon/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prodcon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boundedbuffer buf = new Boundedbuffer(4);
+
+            Producer prod = new Producer(10);
+            Consumer con = new Consumer();
+
+            Parallel.Invoke(prod.run(), con.run());
+        }
+    }
+}
